d2da7d1bf2a14786bd76016d5dc4df300d8b8f8a
uberj <uberj@warrior.hsd1.or.comcast.net>
2011-05-02 21:21:11 -0700

    trying to import functions dynamically. I think i've got it. Need to do homework now.

--- brain.py
+++ brain.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 # Database logging
 import sqlite3
-# Plugin design by Rob McGuire-Dale
 from plugins.ip import get_ip
 from plugins.utils import s,spell,auto
 from plugins.jokes import troll
@@ -9,27 +8,57 @@ from plugins.jokes import troll
 # util libraries
 import re
 import time
+from ConfigParser import ConfigParser
 
-# This should eventually be migrated to a config file
-DATABASE = "/home/uberj/derp/db/channel_logs_db"
+# Need to make this absolute eventually
+PLUGIN_CONFIG = 'config/plugins.cfg'
+
 class Brain:
     def __init__(self,channel_log,channel):
         self.thoughts={}
-        self.channel_log=channel_log
+        self.plugins={}
+        self.p_config = ConfigParser()
+        print self.p_config
+        #self.channel_log=channel_log
+        #self.load_plugins()
+        #self.conn = sqlite3.connect(DATABASE)
+        #self.cursor = self.conn.cursor()
+        #print "Going to use "+channel
+        #self.cursor.execute("""create table if not exists %s
+        #                        (date text, channel text, user text, msg text)
+        #                    """%(channel))
+
+    def parse_config(self):
+        self.p_config.read(PLUGIN_CONFIG)
+        plugins = self.p_config.sections()
+        for plugin in plugins:
+            # function called for plugin command
+            action = self.p_config.options(plugin)
+            self.plugins[plugin]=[action[0],self.p_config.get(plugin,action[0])]
+
+        print self.plugins
         self.load_plugins()
-        self.conn = sqlite3.connect(DATABASE)
-        self.cursor = self.conn.cursor()
-        print "Going to use "+channel
-        self.cursor.execute("""create table if not exists %s
-                                (date text, channel text, user text, msg text)
-                            """%(channel))
+
+
+
 
     def load_plugins(self):
+        for idea,action in self.plugins.items():
+            __import__('plugins.ip',fromlist=['get_ip'])
+            module = action[1][:-3]
+            function = action[0]
+            module_path = "plugins.%s." % module
+            print module
+            print function
+            print module_path
+            self.thoughs[idea] = __import__(module_path).module.function
+        """
         self.thoughts['.ip']    = get_ip
         self.thoughts['.s']     = s
         self.thoughts['.spell'] = spell
         self.thoughts['.autoc'] = auto
         self.thoughts['.troll'] = troll
+        """
 
     def contemplate(self,protocol,user,channel,msg):
         channel = re.sub('#+','',channel)
